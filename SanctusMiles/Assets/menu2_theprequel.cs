50118a7667ecfcae89c7f956ede1cd6bf533cbcc
3ldins <91253537+3ldins@users.noreply.github.com>
2022-04-25 12:00:16 +0200

    menu animations

---
+++ SanctusMiles/Assets/menu2_theprequel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu2_theprequel : MonoBehaviour
+{
+
+    public GameObject playbuttons;
+    public Animator playbuttonsAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayMenu()
+    {
+        playbuttons.SetActive(true);
+        playbuttonsAnimator.Play("ClickPlay_mainbuttonsmove");
+    }
+}
