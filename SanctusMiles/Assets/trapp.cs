d62d9dfac839448999cc65753532371792801920
3ldins <91253537+3ldins@users.noreply.github.com>
2022-05-24 10:47:11 +0200

    trapp funcionality added, spele e literally playable nå

---
+++ SanctusMiles/Assets/trapp.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class trapp : MonoBehaviour
+{
+
+    public string nextscene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player Hurtbox")
+        {
+            Debug.Log("nextttt");
+            SceneManager.LoadScene(nextscene);
+        }
+
+    }
+}
