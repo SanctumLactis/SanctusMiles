14bb8fcd11fa6bdaa17731989f7905452b00b892
3ldins <91253537+3ldins@users.noreply.github.com>
2022-05-25 16:36:35 +0200

    big changes les desc for info
    added key mechanic i lvl2
    added all sfx i THINK
    level & hierarchy clean ups
    bytta player boxcollider te circle
    yayayaya

--- SanctusMiles/Assets/zomberSFX.cs
+++ SanctusMiles/Assets/zomberSFX.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class zomberSFX : MonoBehaviour
 {
 
-    public AudioSource audioSourceGrowl;
+    private AudioSource audioSourceGrowl;
+    public float minWaitBetweenPlays = 1f;
+    public float maxWaitBetweenPlays = 5f;
+    public float waitTimeCountdown = -1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSourceGrowl = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //audioSourceGrowl.Play([Random.Range(0, 10000)];);
+
+        if (!audioSourceGrowl.isPlaying)
+        {
+            if (waitTimeCountdown < 0f)
+            {
+                audioSourceGrowl.Play();
+                waitTimeCountdown = Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
+            }
+            else
+            {
+                waitTimeCountdown -= Time.deltaTime;
+            }
+        }
     }
 }
